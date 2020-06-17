1e3c83a6a17965f1b241df8127e71d9de3ef66b5
Nilesh Jain <njain.we@gmail.com>
2020-06-17 23:47:58 +0530

    resubmit fixed

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
 from .models import List
 from .forms import ListForm
 from django.contrib import messages
@@ -21,3 +21,36 @@ def home(request):
 
 def about(request):
 	return render(request, 'about.html',{})
+
+def delete(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.delete()
+	messages.success(request,('The item has been deleted'))
+	return redirect('home')
+
+def cross_off(request,list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = True
+	item.save()
+	return redirect('home')
+
+def uncross(request,list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = False
+	item.save()
+	return redirect('home')	
+
+def edit(request,list_id):
+	if request.method== 'POST':
+		item = List.objects.get(pk=list_id)
+
+		form = ListForm(request.POST or none,instance=item)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Items has been edited')
+			return redirect('home')
+
+	else:
+		item = List.objects.get(pk=list_id)
+		return render(request, 'edit.html',{'item':item})
