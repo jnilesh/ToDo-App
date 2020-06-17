47ced7abbb96a9911a92884385a74acac99e60f4
Nilesh Jain <njain.we@gmail.com>
2020-06-17 22:34:11 +0530

    resubmit fixed

--- todo_list/views.py
+++ todo_list/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import List
 from .forms import ListForm
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
@@ -11,9 +12,8 @@ def home(request):
 
 		if form.is_valid():
 			form.save()
-			all_items = List.objects.all
-			messages.success(request, ('Item Added to the List!'))
-			return render(request, 'home.html',{'all_items':all_items , 'messages':'Item Added to the List!' })
+			messages.success(request, 'Your password has been changed successfully!')
+			return HttpResponseRedirect(request.path_info)
 
 	else:
 		all_items = List.objects.all
