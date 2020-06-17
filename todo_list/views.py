e32c7756f871d664bbd0a5fe0dbca18346338d4f
Nilesh Jain <njain.we@gmail.com>
2020-06-17 21:01:55 +0530

    added functionality

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+
+	if request.method== 'POST':
+		form = ListForm(request.POST or none)
+
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			messages.success(request, ('Item Added to the List!'))
+			return render(request, 'home.html',{'all_items':all_items , 'messages':'Item Added to the List!' })
+
+	else:
+		all_items = List.objects.all
+		return render(request, 'home.html',{'all_items':all_items})
+
+def about(request):
+	return render(request, 'about.html',{})
